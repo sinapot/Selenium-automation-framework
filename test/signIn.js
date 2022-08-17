a1e5221352234da6f327be3d4a60fd6366e331a1
sinapot <omai_edison@yahoo.com>
2022-08-17 17:50:15 +1200

    all working test with Mocha and POM

--- test/signIn.js
+++ test/signIn.js
@@ -1,17 +1,15 @@
 const { expect } = require('chai');
-const { Builder } = require('selenium-webdriver');
+const { By, Builder } = require('selenium-webdriver');
 require('chromedriver')
-const { HomePage } = require('../pageObjects/homePage')
-const { SignInPage } = require('../pageObjects/signInPage')
-const { MyAccountPage } =  require('../pageObjects/myAccountPage')
+const { HomePage } = require('../pageObjects/homePage');
+const { SignInPage } = require('../pageObjects/signInPage');
 
-describe('SignIn', function () {
+describe('Sign In', function () {
 
     let driver;
     before(async function () {
         //Start the session
         driver = await new Builder().forBrowser('chrome').build()
-        //await driver.get('http://automationpractice.com/index.php?controller=authentication&back=my-account');
         await driver.get('http://automationpractice.com/index.php');
     })
 
@@ -20,24 +18,16 @@ describe('SignIn', function () {
         await driver.quit();
     });
 
-    it('login with Page Object', async () => {
+    it('Sign In', async () => {
 
         const homePage = new HomePage(driver);
-        const signInPage = new SignInPage(driver)
-        //const myAccountPage = new MyAccountPage(driver);
-        
-        //click sign in button from home page
-        await homePage.signInButton.click();
-        
-        //type email, password then click sign in button
-        await signInPage.email.sendKeys('alon@sharklasers.com');
-        await signInPage.password.sendKeys('alonalon')
-        await signInPage.signInButton.click();
-        
-        //get page title
-        //const pageTitle = await driver.getTitle()
-        //await expect(pageTitle).to.equal('My account - My Store');  
+        const signInPage = new SignInPage(driver);
 
+        //search blouse and click to go to next page
+        await homePage.signInButton.click();
+        // await signInPage.email.sendKeys('alon@sharklasers.com');
+        // await signInPage.password.sendKeys('alonalon')
+        // await signInPage.signInButton.click();
 
     });
 });
